0dbfe2140734ac4195ad2ed96227b5362e9cb614
Setharika Sok <>
2023-08-11 00:15:24 -0400

    Update get users routes.

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,7 +4,7 @@ import { userController } from "../controllers/user.controller";
 const routes = Router();
 
 routes.post("/login", userController.loginUser)
-routes.get("/login", userController.index)
+routes.get("/", userController.index)
 routes.get("/dashboard", userController.authmiddleware, userController.dashboard)
 routes.post("/", userController.createUser);
 routes.get("/:id", userController.findUniqueUser);
